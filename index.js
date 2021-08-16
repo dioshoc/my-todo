ab4f9a1aea4c9bbab168a26a6cced5b5d9116354
Mikalai Gryshuk <nikolai.grishuk@intexsoft.by>
2021-08-16 11:32:47 +0300

    Add last change.

--- index.js
+++ index.js
@@ -3,38 +3,45 @@ const inputFormAddTask = document.querySelector(".form__input input");
 const textareaFormAddTask = document.querySelector(".form__textarea textarea");
 const openPopupButton = document.querySelector(".add-task__button");
 const closePopupButton = document.querySelector(".popup__close");
-const formSubmitButton = document.querySelector('.form__submit-btn')
+const formSubmitButton = document.querySelector('.form__submit-btn');
+
+function removePopup() {
+  popup.classList.remove("popup_active");
+}
 
 // Open and Close popup
 openPopupButton.addEventListener("click", () => {
   popup.classList.add("popup_active");
+  this.setAttribute("active");
 });
 
 closePopupButton.addEventListener("click", () => {
-  popup.classList.remove("popup_active");
+  removePopup();
 });
 
-popup.addEventListener("click", function () {
+popup.addEventListener("click", function (elem) {
   if (!!elem.target.querySelector(".popup__content")) {
-    popup.classList.remove("popup_active");
+    removePopup();
   }
 });
 
-window.addEventListener("keydown", function (e){
+window.addEventListener("keydown", function (e) {
   if (popup.classList.contains("popup_active")) {
     if (e.keyCode === 27) {
-      popup.classList.remove("popup_active");
+      removePopup();
     }
   }
-})
-// Check form
+});
+
+// Check form on string length
 inputFormAddTask.addEventListener("input", function () {
   if (inputFormAddTask.value !== "" && inputFormAddTask.value.length >= 5) {
     formSubmitButton.removeAttribute("disabled");
   }
 });
-textareaFormAddTask.addEventListener('onclick', function(){
-  if(this.scrollTop > 0){
+
+textareaFormAddTask.addEventListener('input', function () {
+  if (this.scrollTop > 0) {
     this.style.height = this.scrollHeight + "px";
   }
 });
