11501e025aab684a3e045faaf434270cd8b0f87c
Mikalai Gryshuk <nikolai.grishuk@intexsoft.by>
2021-08-16 14:09:04 +0300

    Sprint 3, finish project.

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 const popup = document.querySelector(".popup");
-const inputFormAddTask = document.querySelector(".form__input input");
+const inputFormAddTask = document.querySelectorAll(".form__input input");
 const textareaFormAddTask = document.querySelector(".form__textarea textarea");
 const openPopupButton = document.querySelector(".add-task__button");
 const closePopupButton = document.querySelector(".popup__close");
-const formSubmitButton = document.querySelector('.form__submit-btn');
+const formSubmitButton = document.querySelector(".form__submit-btn");
+const taskList = document.querySelector(".todo__list");
+const taskBody = document.querySelector(".template .todo-item");
+let taskRemove = document.querySelectorAll(".todo-item .todo-item__remove-btn");
 
+// Remove task
+function removeBtnActive() {
+  taskRemove = document.querySelectorAll(".todo-item .todo-item__remove-btn");
+  taskRemove.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      this.parentElement.remove()
+    });
+  });
+}
+
+// Open and Close popup
 function removePopup() {
   popup.classList.remove("popup_active");
 }
 
-// Open and Close popup
 openPopupButton.addEventListener("click", () => {
   popup.classList.add("popup_active");
 });
-
 closePopupButton.addEventListener("click", () => {
   removePopup();
 });
-
-popup.addEventListener("click", function (elem) {
+popup.addEventListener("click", (elem) => {
   if (!!elem.target.querySelector(".popup__content")) {
     removePopup();
   }
 });
-
-window.addEventListener("keydown", function (e) {
+window.addEventListener("keydown", (e) => {
   if (popup.classList.contains("popup_active")) {
     if (e.keyCode === 27) {
       removePopup();
     }
-  }
-});
-
-// Check form on string length
-inputFormAddTask.addEventListener("input", function () {
-  if (inputFormAddTask.value !== "" && inputFormAddTask.value.length >= 5) {
-    formSubmitButton.removeAttribute("disabled");
   }
 });
 
@@ -45,8 +48,33 @@ textareaFormAddTask.addEventListener('input', function () {
   }
 });
 
-document.forms.addTodo.onsubmit = function() {
-  let message = this.todoTitle.value;
-  console.log(message)
+inputFormAddTask.forEach((element, id) => {
+  element.addEventListener("input", function () {
+    if (element.value !== "" && element.value.length >= 5) {
+      formSubmitButton.removeAttribute("disabled");
+    }
+  });
+  if (element.required) {
+    const requiredCE = document.createElement("span")
+    requiredCE.classList.add("form__required")
+    requiredCE.prepend("*")
+    element.parentElement.querySelector('label').append(requiredCE)
+  }
+});
+
+// Form submit
+document.forms.addTask.onsubmit = function () {
+  const addChild = taskBody.cloneNode(true);
+
+  addChild.querySelector('.todo-item__title').prepend(this.taskTitle.value);
+  addChild.querySelector('.todo-item__subtitle').prepend(this.taskSubtitle.value);
+  taskList.append(addChild);
+
+  this.taskTitle.value = "";
+  this.taskSubtitle.value = "";
+
+  formSubmitButton.setAttribute("disabled", "true");
+  removeBtnActive();
   return false;
 };
+
